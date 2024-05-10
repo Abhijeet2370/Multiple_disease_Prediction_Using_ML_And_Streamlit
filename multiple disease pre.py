c0d93380fa655b6416014aec6653f63c31aef3b9
Abhijeet2370 <119788761+Abhijeet2370@users.noreply.github.com>
2024-05-10 08:48:37 +0530

    Update multiple disease pre.py

--- multiple disease pre.py
+++ multiple disease pre.py
@@ -11,17 +11,13 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('
-Multiple_disease_Prediction_Using_ML_And_Streamlit/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('
-Multiple_disease_Prediction_Using_ML_And_Streamlit/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('
-Multiple_disease_Prediction_Using_ML_And_Streamlit/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
 
-Covid_19_model = pickle.load(open('
-Multiple_disease_Prediction_Using_ML_And_Streamlit/saved models/Covid_19_model.sav', 'rb'))
+Covid_19_model = pickle.load(open('saved models/Covid_19_model.sav', 'rb'))
 
 
 
